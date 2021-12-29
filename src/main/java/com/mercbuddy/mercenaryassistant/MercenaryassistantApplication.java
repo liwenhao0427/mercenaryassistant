8c115df99c6b97a4b79d6a7b96e930f12bcd846b
李文浩 <liwenhao0427@163.com>
2021-12-29 09:55:49 +0800

    init

---
+++ src/main/java/com/mercbuddy/mercenaryassistant/MercenaryassistantApplication.java
@@ -0,0 +1,13 @@
+package com.mercbuddy.mercenaryassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercenaryassistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MercenaryassistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mercbuddy/mercenaryassistant/MercenaryassistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercbuddy.mercenaryassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercenaryassistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
